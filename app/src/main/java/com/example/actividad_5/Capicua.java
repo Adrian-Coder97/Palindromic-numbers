923836f6d826ea1efa4c7988204e2c7227c2d742
Adrian-Coder97 <adrian_lch@outlook.com>
2021-02-18 01:24:01 -0600

    Navegacion completada y capicua solo se llego hasta 2 digitos

--- app/src/main/java/com/example/actividad_5/Capicua.java
+++ app/src/main/java/com/example/actividad_5/Capicua.java
@@ -3,20 +3,78 @@ package com.example.actividad_5;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.sql.SQLOutput;
 
 public class Capicua extends AppCompatActivity {
+
+    TextView num, res;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_capicua);
+
+        num = (TextView) findViewById(R.id.txtNumero);
+        res = (TextView) findViewById(R.id.txtResultado);
+
+
     }
 
-    public void Anterior(View view){
-        Intent anterior = new Intent(this,Menu.class);
+    public void Anterior(View view) {
+        Intent anterior = new Intent(this, Menu.class);
         startActivity(anterior);
 
     }
+
+    public void Evaluar(View view) {
+        negro();
+        String valor = num.getText().toString();
+        if (valor.isEmpty()) {
+            num.setError("llenar este campo");//NO PONER MAYUSCULAS
+        }
+        if (!valor.isEmpty()) {
+            int valorInt = Integer.parseInt(valor);
+            String nume = String.valueOf(valorInt);
+            String resul;
+            if (valor.length() == 1) {
+                verde();
+                resul = ("El numero " + nume + " si es capicua ya que todos los numeros de un digito son numeros capicua");
+                res.setText(resul);
+            } else if (valor.length() == 2) {
+                if (valor.charAt(0) == valor.charAt(1)) {
+                    verde();
+                    resul = ("El numero " + nume + " si es capicua ya que el primer digito es igual al segundo");
+                    res.setText(resul);
+                } else if (valor.charAt(0) != valor.charAt(1)) {
+                    rojo();
+                    resul = ("El numero " + nume + " no es capicua ya que el primer digito NO es igual al segundo");
+                    res.setText(resul);
+                }
+            }
+        }
+
+    }
+
+    public void negro() {
+        res.setTextColor(Color.BLACK);
+        //res.setShadowLayer(1, 0, 0, Color.BLACK);
+    }
+
+    public void verde() {
+        res.setTextColor(Color.BLUE);
+        //res.setShadowLayer(1, 0, 0, Color.BLACK);
+    }
+
+    public void rojo() {
+        res.setTextColor(Color.RED);
+        //res.setShadowLayer(1, 0, 0, Color.BLACK);
+    }
+
+
 }
--- app/src/main/java/com/example/actividad_5/Posiciones.java
+++ app/src/main/java/com/example/actividad_5/Posiciones.java
@@ -6,20 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class Menu extends AppCompatActivity {
+public class Posiciones extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_posiciones);
     }
 
-    public void Siguiente(View view){
-        Intent intent = new Intent(this,Capicua.class);
-        startActivity(intent);
-    }
-    public void Siguiente1(View view){
-        Intent intent = new Intent(this,Posiciones.class);
+    public void VolverDePosiciones(View view){
+        Intent intent = new Intent(this,Menu.class);
         startActivity(intent);
     }
 }
